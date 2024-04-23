e969ffd0a21c6b81457a71b7095932050a59120f
^Amin Aenurahman <Shadow47_@msn.com>
2024-04-23 13:40:43 +0700

    Add files via upload

--- data_perusahaan.php
+++ data_perusahaan.php
@@ -16,40 +16,41 @@
 	<div class="container">
 		
 		<h1 class="mt-3">Form Pendaftaran Perusahaan</h1>
-		
-		<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Perusahaan</th>
-					<th scope="col">Tlp</th>
-					<th scope="col">Email</th>
-					<th scope="col">Domain</th>
-					<th scope="col">Dokumen</th>
-					<th scope="col">Sektor Perusahaan</th>
-					<th scope="col">Add On</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					$i=1;
-					$tp=mysqli_query($cn, "SELECT * FROM web_registration");
-					while($r=mysqli_fetch_array($tp)){
-				?>
-				<tr>
-					<th scope="row"><?php echo $i ?></th>
-					<td><?php echo $r['id_perusahaan'] ?> - <?php echo $r['nama_perusahaan'] ?></td>
-					<td><?php echo $r['no_tlp'] ?></td>
-					<td><?php echo $r['email'] ?></td>
-					<td><?php echo $r['domain_website'] ?></td>
-					<td><?php echo $r['dokumen_perizinan'] ?></td>
-					<td><?php echo $r['sektor_perusahaan'] ?></td>
-					<td><?php echo $r['addOn'] ?></td>
-				</tr>
-				<?php $i=$i+1;?>
-				<?php } ?>
-			</tbody>
-		</table>
+		<div class="table-responsive">
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Perusahaan</th>
+						<th scope="col">Tlp</th>
+						<th scope="col">Email</th>
+						<th scope="col">Domain</th>
+						<th scope="col">Dokumen</th>
+						<th scope="col">Sektor Perusahaan</th>
+						<th scope="col">Add On</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						$i=1;
+						$tp=mysqli_query($cn, "SELECT * FROM web_registration");
+						while($r=mysqli_fetch_array($tp)){
+					?>
+					<tr>
+						<th scope="row"><?php echo $i ?></th>
+						<td><?php echo $r['id_perusahaan'] ?> - <?php echo $r['nama_perusahaan'] ?></td>
+						<td><?php echo $r['no_tlp'] ?></td>
+						<td><?php echo $r['email'] ?></td>
+						<td><?php echo $r['domain_website'] ?></td>
+						<td><?php echo $r['dokumen_perizinan'] ?></td>
+						<td><?php echo $r['sektor_perusahaan'] ?></td>
+						<td><?php echo $r['addOn'] ?></td>
+					</tr>
+					<?php $i=$i+1;?>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
 		
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
